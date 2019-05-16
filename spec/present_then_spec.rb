0fbdb5d8b2eff99c08a68e82c6efec2cb4d466f4
oieioi <atsuinatsu.samuifuyu@gmail.com>
2019-05-16 17:49:35 +0900

    Add tests

--- spec/present_then_spec.rb
+++ spec/present_then_spec.rb
@@ -1,9 +1,30 @@
+# frozen_string_literal: true
+
 RSpec.describe PresentThen do
-  it "has a version number" do
+  it 'has a version number' do
     expect(PresentThen::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'Object#present_then(&block)' do
+    subject { val.present_then(&block) }
+    let(:val) {}
+    let(:block) { ->(val) { "given val is #{val}" } }
+
+    context 'when object is blank' do
+      let(:val) { nil }
+      it { is_expected.to eq nil }
+    end
+
+    context 'when object is present' do
+      let(:val) { 1 }
+      context 'and give a block' do
+        it { is_expected.to eq 'given val is 1' }
+      end
+
+      context 'and do not give a block' do
+        let(:block) { nil }
+        it { is_expected.to eq 1 }
+      end
+    end
   end
 end
